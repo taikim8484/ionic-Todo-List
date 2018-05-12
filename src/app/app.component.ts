c58687d29cb4393a73007979f4d7961a8434a976
Kim Huu Tai <liemkhiet1010@gmail.com>
2018-05-12 15:11:20 +0700

    clear modify view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,7 @@ export class MyApp {
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      
     });
   }
 }
